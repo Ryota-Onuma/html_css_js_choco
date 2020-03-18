ea44eb9509ad09cd092f6b2249ab77fce43205f5
Ryota <onumaryotajh@gmail.com>
2020-03-18 15:57:30 +0900

    first-commit

---
+++ template/template.js
@@ -0,0 +1,486 @@
+const base = '<div id="template"><div id="chocorate"></div></div>'
+const template = document.getElementById('template')
+template.outerHTML = base
+
+
+
+
+
+
+
+const node = '"<div id="grass_container">' +
+"<div id='grass0'>"+"</div>"+ 
+"<div id='grass1'>"+"</div>"+ 
+"<div id='grass2'>"+"</div>"+ 
+"<div id='grass3'>"+"</div>"+ 
+"<div id='grass4'>"+"</div>"+ 
+"<div id='grass5'>"+"</div>"+ 
+"<div id='grass6'>"+"</div>"+ 
+"<div id='grass7'>"+"</div>"+ 
+"<div id='grass8'>"+"</div>"+ 
+"<div id='grass9'>"+"</div>"+ 
+"<div id='grass11'>"+"</div>"+ 
+"<div id='grass12'>"+"</div>"+ 
+"<div id='grass13'>"+"</div>"+ 
+"<div id='grass14'>"+"</div>"+ 
+"<div id='grass15'>"+"</div>"+ 
+"<div id='grass16'>"+"</div>"+ 
+"<div id='grass17'>"+"</div>"+ 
+"<div id='grass18'>"+"</div>"+ 
+"</div>"+ 
+'<div id="header_container">' +
+"<div id='header'>" +
+"<div id='choco'>" +
+ "<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon" +
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+"<div id='choco'>"+
+ "<div class='choco1'>"+
+"<div class='choco2'>"+
+"<div class='choco3'>"+
+ "<div class='choco4'>GeekSalon"+
+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+"</div>"+ 
+
+"<div id='choco'>"+
+ "<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon" +
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+"<div id='choco'>"+
+ "<div class='choco1'>" +
+"<div class='choco2'>" + 
+"<div class='choco3'>" + 
+ "<div class='choco4'>GeekSalon" +
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+"<div id='choco'>"+
+ "<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+ "<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+"<div id='choco'>"+
+ "<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+ "<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+ "<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+"<div id='choco'>"+
+ "<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"<div id='header'>" +
+"<div id='choco'>" +
+ "<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon" +
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+"<div id='choco'>"+
+ "<div class='choco1'>"+
+"<div class='choco2'>"+
+"<div class='choco3'>"+
+ "<div class='choco4'>GeekSalon"+
+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+"</div>"+ 
+
+"<div id='choco'>"+
+ "<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon" +
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+"<div id='choco'>"+
+ "<div class='choco1'>" +
+"<div class='choco2'>" + 
+"<div class='choco3'>" + 
+ "<div class='choco4'>GeekSalon" +
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+"<div id='choco'>"+
+ "<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+ "<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+"<div id='choco'>"+
+ "<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+ "<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+"<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"<div id='choco'>"+
+ "<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+"<div id='choco'>"+
+ "<div class='choco1'>" +
+"<div class='choco2'>" +
+"<div class='choco3'>" +
+ "<div class='choco4'>GeekSalon"+
+
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+"</div>"+ 
+ "</div>"+ 
+"</div>"+ 
+ "</div>"
+
+window.onload = function(){
+const chocorate= document.getElementById('chocorate')
+chocorate.outerHTML = node
+ }
+
+
+
